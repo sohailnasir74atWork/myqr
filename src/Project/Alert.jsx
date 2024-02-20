400f37720a8198491549d79734f9908982e6f221
Ahmed Waqas <waqasnasir818@gmail.com>
2024-02-20 16:39:47 +0500

    commit

--- src/Project/Alert.jsx
+++ src/Project/Alert.jsx
@@ -14,60 +14,11 @@ const navigate = useNavigate()
   const handleClose = () => {
     setOpenAlert(false);
      };
-
-  const handleAgree = () => {
-    setOpenAlert(false);
-    setActiveStep(0)
-    setQrCodeSettings({
-        type:'',
-        qrName: "My QR",
-        size: { height: "300", width: "300" },
-        inputData: {
-          url: { value: null },
-          text: { value: null },
-          mail: { email: null, message : null },
-          whatsapp: { number: null, message : null },
-          message: { number: null, message : null },
-          call: { call : null },
-          wifi: { networkName : null, networkType : null, password : null, isHide : false },
-          vcard: {firstName: null,
-            lastName: null,
-            phoneNumber: null,
-            mobile : null,
-            email: null,
-            website: null,
-            company: null,
-            jobTitle: null,
-            address: null,
-            fax: null,
-            city: null,
-            postalCode: null,
-            country: null}
-       },
-        logo: null,
-        logoSetting: { backgrounddots: true, margin: 10 },
-        colors: {
-          background: { isSolid: true, color: "#FFFFFF" },
-          dots: { isSolid: true, color: "#000000" },
-          square: { isSolid: true, color: "#000000" },
-          cornerDots: { isSolid: true, color: "#000000" },
-        },
-        types: {
-          corner: { type: "square" },
-          dots: { type: "square" },
-          square: { isSolid: true, color: "#000000" },
-          cornerDots: { type: "square" },
-        },
-        clearInput: false,
-      }) 
-      navigate('/create')
- };
-
-  return (
+ return (
     <React.Fragment>
       
       <div>
-      <Snackbar open={openAlert} autoHideDuration={600000} onClose={handleClose}>
+      <Snackbar open={openAlert} autoHideDuration={60000} onClose={handleClose}>
         <Alert
           onClose={handleClose}
           severity="error"
