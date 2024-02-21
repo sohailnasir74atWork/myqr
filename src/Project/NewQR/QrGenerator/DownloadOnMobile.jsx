1c95d04cace79593b8087ad9069ba335b7b30c56
Ahmed Waqas <waqasnasir818@gmail.com>
2024-02-21 20:44:17 +0500

    COMMIT

--- src/Project/NewQR/QrGenerator/DownloadOnMobile.jsx
+++ src/Project/NewQR/QrGenerator/DownloadOnMobile.jsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import EditIcon from '@mui/icons-material/Edit';
 import Divider from '@mui/material/Divider';
+import EditIcon from '@mui/icons-material/Edit';
 import ArchiveIcon from '@mui/icons-material/Archive';
 import FileCopyIcon from '@mui/icons-material/FileCopy';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const StyledMenu = styled((props) => (
@@ -28,37 +27,27 @@ const StyledMenu = styled((props) => (
     borderRadius: 6,
     marginTop: theme.spacing(1),
     minWidth: 180,
-    color:
-      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-    boxShadow:
-      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    color: theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+    boxShadow: 'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
     '& .MuiMenu-list': {
       padding: '4px 0',
-    },
-    '& .MuiMenuItem-root': {
-      '& .MuiSvgIcon-root': {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-      },
-      '&:active': {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity,
-        ),
-      },
     },
   },
 }));
 
 export default function DownloadOnMobile() {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [selectedFormat, setSelectedFormat] = React.useState('PNG');
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setAnchorEl(null);
+  };
+  const handleSelect = (format) => {
+    setSelectedFormat(format);
+    handleClose();
   };
 
   return (
@@ -70,11 +59,11 @@ export default function DownloadOnMobile() {
         aria-expanded={open ? 'true' : undefined}
         variant="contained"
         disableElevation
-        onClick={handleClick}
-        endIcon={<KeyboardArrowDownIcon />}
-        sx={{borderRadius:'50px'}}
+        // onClick={'handleClick'}
+        endIcon={<KeyboardArrowDownIcon onClick={handleClick} />}
+        sx={{ display: 'flex', justifyContent: 'space-between', width: 'auto', borderRadius: '50px' }}
       >
-        Download
+        {selectedFormat} Download
       </Button>
       <StyledMenu
         id="demo-customized-menu"
@@ -85,18 +74,24 @@ export default function DownloadOnMobile() {
         open={open}
         onClose={handleClose}
       >
-        <MenuItem onClick={handleClose} disableRipple>
+        <MenuItem onClick={() => handleSelect('SVG')} disableRipple>
           <EditIcon />
           SVG
         </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
+        <Divider />
+        <MenuItem onClick={() => handleSelect('PNG')} disableRipple>
           <FileCopyIcon />
           PNG
         </MenuItem>
-        <Divider sx={{ my: 0.5 }} />
-        <MenuItem onClick={handleClose} disableRipple>
+        <Divider />
+        <MenuItem onClick={() => handleSelect('WEBP')} disableRipple>
           <ArchiveIcon />
           WEBP
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={() => handleSelect('JPEG')} disableRipple>
+          <ArchiveIcon />
+          JPEG
         </MenuItem>
       </StyledMenu>
     </div>
