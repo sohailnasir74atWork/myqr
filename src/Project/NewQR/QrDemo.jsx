b741b57214e5dbd51122f63cd7738751fdc43de3
Ahmed Waqas <waqasnasir818@gmail.com>
2024-02-22 16:19:11 +0500

    commit

--- src/Project/NewQR/QrDemo.jsx
+++ src/Project/NewQR/QrDemo.jsx
@@ -1,35 +1,31 @@
 import * as React from "react";
-import PropTypes from "prop-types";
-import { useTheme } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import mobileFrame from "../../Assets/mobileFrame.webp";
 import QrGenerator from "./QrGenerator/QrGenerator";
 import './newqrStyle.css'
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function QrDemo({ prop }) {
   const { qrCodeSettings } = prop;
-  const [value, setValue] = React.useState(0);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  const handleChangeIndex = (index) => {
-    setValue(index);
-  };
+  const heading = () => {
+    const path = location.pathname.toLowerCase();
+    
+    if (path.includes('input') || path.includes('design')) {
+      return `QR Code for ${qrCodeSettings.type}`;
+    } else if (path.includes('create')) {
+      return 'Preview';
+    } 
+  }
 
   return (
-    <div className="demo-container" >
+    <div className="demo-container">
       <div className="types-of-qr-mobile-demo">
         <div className="heading-container" style={{ width: "100%" }}>
-          <span className="heading-2 center">Preview</span>
+          <span className="heading-2 center">{heading()}</span>
         </div>
         <div className="mobile-frame">
-         <div className="demo-preview">
-            {" "}
+          <div className="demo-preview">
             <QrGenerator prop={{ qrCodeSettings }} />
           </div>
         </div>
