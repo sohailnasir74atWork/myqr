699fc551aa4bbb9dbfba0ee6f5b01f6cfb138921
Ahmed Waqas <waqasnasir818@gmail.com>
2024-02-22 19:26:57 +0500

    commit

--- src/Project/NewQR/QrGenerator/DownloadOnMobile.jsx
+++ src/Project/NewQR/QrGenerator/DownloadOnMobile.jsx
@@ -25,7 +25,7 @@ const StyledMenu = styled((props) => (
   '& .MuiPaper-root': {
     borderRadius: 6,
     marginTop: theme.spacing(1),
-    minWidth: 150,
+    minWidth: 135,
     color: theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
     boxShadow: 'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
     '& .MuiMenu-list': {
@@ -86,18 +86,18 @@ const disabled = () => pathname.includes('design');
           SVG
           <CloudDownloadIcon />
         </MenuItem>
-        <Divider />
+        <Divider sx={{ margin: '1px 0 !important' }} />
         <MenuItem onClick={() => handleSelect('PNG')} disableRipple sx={{ display: 'flex', justifyContent: 'space-between' }}>
           PNG
           <CloudDownloadIcon />
         </MenuItem>
-        <Divider />
-        <MenuItem onClick={() => handleSelect('WEBP')} disableRipple sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Divider sx={{ margin: '1px 0 !important' }} />
+       <MenuItem onClick={() => handleSelect('WEBP')} disableRipple sx={{ display: 'flex', justifyContent: 'space-between' }}>
           WEBP
           <CloudDownloadIcon />
         </MenuItem>
-        <Divider />
-        <MenuItem onClick={() => handleSelect('JPEG')} disableRipple sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Divider sx={{ margin: '1px 0 !important' }} />
+       <MenuItem onClick={() => handleSelect('JPEG')} disableRipple sx={{ display: 'flex', justifyContent: 'space-between' }}>
           JPEG
           <CloudDownloadIcon />
         </MenuItem>
