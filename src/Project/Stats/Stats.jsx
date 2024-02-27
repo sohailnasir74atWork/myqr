b3b494c27a14ca0ce8fe206edf321f932269bdc6
Ahmed Waqas <waqasnasir818@gmail.com>
2024-02-27 16:35:42 +0500

    commit

--- src/Project/Stats/Stats.jsx
+++ src/Project/Stats/Stats.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import commingsoon from "../../Assets/comming soon/comingsoon.png"
-import './statsStyles.css'
+import '../MyQR/commingsoon.css'
 const Stats = () => {
-  return <div className='container'>
-    <div className='container-commingsoon'>
-    <img src={commingsoon} alt='comming soom'/>
+  return <div className="container" style={{ backgroundColor: "white" }}>
+  <div className="container-commingsoon">
+    <img src={commingsoon} alt="comming soom" />
+    <div className="fade-in-out">
+      {" "}
+      <span>The Pro version is scheduled to go live in mid-2024</span>{" "}
     </div>
-  </div>;
+  </div>
+</div>
 }
 
 export default Stats
