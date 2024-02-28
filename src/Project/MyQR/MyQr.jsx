669a333dc54be4b0b75b336ae752903ada2f2411
Ahmed Waqas <waqasnasir818@gmail.com>
2024-02-28 15:57:10 +0500

    commit

--- src/Project/MyQR/MyQr.jsx
+++ src/Project/MyQR/MyQr.jsx
@@ -3,7 +3,7 @@ import commingsoon from "../../Assets/comming soon/comingsoon.png";
 import "./commingsoon.css";
 const Stats = () => {
   return (
-    <div className="container" style={{ backgroundColor: "white" }}>
+    <div style={{ backgroundColor: "white" }}>
       <div className="container-commingsoon">
         <img src={commingsoon} alt="comming soom" />
         <div className="fade-in-out">
--- src/Project/Stats/Stats.jsx
+++ src/Project/Stats/Stats.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import commingsoon from "../../Assets/comming soon/comingsoon.png"
 import '../MyQR/commingsoon.css'
 const Stats = () => {
-  return <div className="container" style={{ backgroundColor: "white" }}>
+  return <div style={{ backgroundColor: "white" }}>
   <div className="container-commingsoon">
     <img src={commingsoon} alt="comming soom" />
     <div className="fade-in-out">
