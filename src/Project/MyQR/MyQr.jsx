3fee76eef0c78efb50d37dee4f25fda02ad42cd9
Ahmed Waqas <waqasnasir818@gmail.com>
2024-03-09 19:54:17 +0500

    commit

--- src/Project/MyQR/MyQr.jsx
+++ src/Project/MyQR/MyQr.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import commingsoon from "../../Assets/comming soon/comingsoon.png";
+// import commingsoon from "../../Assets/comming soon/comingsoon.png";
 import "./commingsoon.css";
 const Stats = () => {
   return (
     <div style={{ backgroundColor: "white" }}>
       <div className="container-commingsoon">
-        <img src={commingsoon} alt="comming soom" />
+        <img src='https://res.cloudinary.com/dgupxycnh/image/upload/v1709656645/qr/comingsoon_cg6z3r.png' alt="comming soom" />
         <div className="fade-in-out">
           {" "}
           <span className="center">The Pro version is scheduled to go live in mid-2024</span>{" "}
