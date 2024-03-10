ef50acdbd846419447a8cb7dbbce262a655ee2bc
Ahmed Waqas <waqasnasir818@gmail.com>
2024-03-10 17:54:05 +0500

    ;lll

--- src/Project/NewQR/QrDemo.jsx
+++ src/Project/NewQR/QrDemo.jsx
@@ -6,7 +6,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 export default function QrDemo({ prop }) {
   const { qrCodeSettings } = prop;
   const location = useLocation();
-  const iframe = location.pathname === 'iframe'
+  const iframe = location.pathname.includes('iframe')
 
 
   const heading = () => {
@@ -21,7 +21,7 @@ export default function QrDemo({ prop }) {
 
   return (
     <div className="demo-container">
-      <div className={iframe ? 'types-of-qr-mobile-demo' : 'types-of-qr-mobile-demo-iframe'}>
+      <div className={iframe ? 'types-of-qr-mobile-demo-iframe' : 'types-of-qr-mobile-demo'}>
         <div className="heading-container" style={{ width: "100%" }}>
           <span className="heading-2 center">{heading()}</span>
         </div>
