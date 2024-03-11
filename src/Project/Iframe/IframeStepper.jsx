e5f8b57dbff70df8b1d5c9a25c3c2cb36f7c7af5
Ahmed Waqas <waqasnasir818@gmail.com>
2024-03-11 21:35:59 +0500

    exdref

--- src/Project/Iframe/IframeStepper.jsx
+++ src/Project/Iframe/IframeStepper.jsx
@@ -1,23 +1,8 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Stepper from "@mui/material/Stepper";
-import Step from "@mui/material/Step";
-import StepLabel from "@mui/material/StepLabel";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import SelectScreen from "../NewQR/SelectTypes";
 import { ImportStats } from "../GlobelStats/GlobelStats";
-import StepperComponent from "../Stepper";
-const steps = [
-  "Select campaign settings",
-  "Create an ad group",
-  "Create an ad",
-];
-
 export default function IframeStepper() {
   
-  const open = false
-  const handleDrawerToggle = false
   const {setActiveStep, activeStep} = ImportStats()
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -27,14 +12,10 @@ export default function IframeStepper() {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
-  const handleReset = () => {
-    setActiveStep(0);
-  };
-
   return (
     <div style={{display:'flex', justifyContent:'space-between'}}>
-    <Button variant="outlined" size="small">Back</Button>
-    <Button variant="contained" size='small'>Next</Button>
+    <Button variant="outlined" size="small" onClick={handleBack} disabled={activeStep === 0}> Back</Button>
+    <Button variant="contained" size='small' onClick={handleNext} disabled={activeStep === 0 || activeStep === 1}>Next</Button>
      
     </div>
   );
