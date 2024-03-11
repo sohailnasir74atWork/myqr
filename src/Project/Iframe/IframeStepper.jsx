3b515d5dd406d5da08765cd23e8859e511fd98d0
Ahmed Waqas <waqasnasir818@gmail.com>
2024-03-11 21:41:25 +0500

    fvf

--- src/Project/Iframe/IframeStepper.jsx
+++ src/Project/Iframe/IframeStepper.jsx
@@ -1,16 +1,24 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 import { ImportStats } from "../GlobelStats/GlobelStats";
+import { useNavigate } from "react-router-dom";
 export default function IframeStepper() {
   
+    const navigate = useNavigate()
   const {setActiveStep, activeStep} = ImportStats()
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
   const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
+    if(activeStep === 1)
+   { navigate('/create')
+    setActiveStep(0)}
+    if(activeStep === 2)
+    {navigate('/create/input')
+    setActiveStep(1)}
+
+  }
 
   return (
     <div style={{display:'flex', justifyContent:'space-between'}}>
