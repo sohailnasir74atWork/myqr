a94955176a72e1a5fe5b2b21b9f2a6ea4acbe0ec
Ahmed Waqas <waqasnasir818@gmail.com>
2024-03-11 22:08:46 +0500

    dcdcd

--- src/Project/Iframe/IframeStepper.jsx
+++ src/Project/Iframe/IframeStepper.jsx
@@ -22,8 +22,8 @@ export default function IframeStepper() {
 
   return (
     <div style={{display:'flex', justifyContent:'space-between'}}>
-    <Button variant="outlined" size="small" onClick={handleBack} disabled={activeStep === 0}> Back</Button>
-    <Button variant="contained" size='small' onClick={handleNext} disabled={activeStep === 0 || activeStep === 1}>Next</Button>
+    <Button variant="outlined" size="small" onClick={handleBack} disabled={activeStep === 0} style={{borderRadius:'50px'}}> Back</Button>
+    <Button variant="contained" size='small' onClick={handleNext} disabled={activeStep === 0 || activeStep === 1} style={{borderRadius:'50px'}}>Next</Button>
      
     </div>
   );
