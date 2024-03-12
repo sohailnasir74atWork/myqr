7cf18f598e1fce0fa992ef8e48c3449d7bb1aac9
Ahmed Waqas <waqasnasir818@gmail.com>
2024-03-12 13:26:42 +0500

    ;lk;lk

--- src/Project/MiniDrawer.jsx
+++ src/Project/MiniDrawer.jsx
@@ -285,9 +285,9 @@ const CommonIconButton = () => (
       </AppBar>
       <Drawer variant="permanent" open={open}  sx={{ position: isMobile ? "absolute" : "" }} className='wordpress'>
         <DrawerHeader>
-          <div style={{height: '60px', display:'flex', justifyContent:'center', alignItems:'center',  width:"100%"}}>
+         {open && <div style={{height: '60px', display:'flex', justifyContent:'center', alignItems:'center',  width:"100%"}}>
             <img src={logo} alt='logo' style={{width:'auto', height:'100%'}}/>
-                    </div>
+                    </div>}
           <IconButton onClick={handleDrawerToggle}>
             {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
           </IconButton>
