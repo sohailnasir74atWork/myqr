ba91db8b527b30464bee3a010c68724aaf710d45
Ahmed Waqas <waqasnasir818@gmail.com>
2024-03-15 05:45:38 +0500

    ;llklk

--- src/App.js
+++ src/App.js
@@ -21,20 +21,20 @@ const theme = createTheme({
       main: "#5956D6",
     },
   },
-  typography: {
-    fontFamily: [
-      "Lato",
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-  },
+  // typography: {
+  //   fontFamily: [
+  //     "Lato",
+  //     "-apple-system",
+  //     "BlinkMacSystemFont",
+  //     '"Segoe UI"',
+  //     '"Helvetica Neue"',
+  //     "Arial",
+  //     "sans-serif",
+  //     '"Apple Color Emoji"',
+  //     '"Segoe UI Emoji"',
+  //     '"Segoe UI Symbol"',
+  //   ].join(","),
+  // },
   components: {
     MuiTypography: {
       styleOverrides: {
